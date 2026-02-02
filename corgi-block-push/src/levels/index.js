e1aadec6d08bd0d42655f6ea904aacb683e8d22a
Brendan Bobryk <babobryk@gmail.com>
2026-02-01 20:14:56 -0800

    redesigned level 4

--- corgi-block-push/src/levels/index.js
+++ corgi-block-push/src/levels/index.js
@@ -66,10 +66,10 @@ const LEVELS = [
     grid: [
       Array(7).fill(null).map(WALL),
 
-      [ WALL(), BLOCK(), WALL(), EMPTY(), EMPTY(), GOAL(), WALL() ],
-      [ WALL(), BLOCK(), WALL(), EMPTY(), BLOCK(), EMPTY(), WALL() ],
-      [ WALL(), EMPTY(), BLOCK(), TREAT(), EMPTY(), EMPTY(), WALL() ],
-      [ WALL(), WALL(), EMPTY(), WALL(), EMPTY(), WALL(), WALL() ],
+      [ WALL(), EMPTY(), WALL(), GOAL(), EMPTY(), EMPTY(), WALL() ],
+      [ WALL(), BLOCK(), EMPTY(), WALL(), WALL(), EMPTY(), WALL() ],
+      [ WALL(), TREAT(), BLOCK(), EMPTY(), BLOCK(), EMPTY(), WALL() ],
+      [ WALL(), WALL(), EMPTY(), WALL(), EMPTY(), EMPTY(), WALL() ],
       [ WALL(), CORGI(), EMPTY(), EMPTY(), BLOCK(), EMPTY(), WALL() ],
 
       Array(7).fill(null).map(WALL),
