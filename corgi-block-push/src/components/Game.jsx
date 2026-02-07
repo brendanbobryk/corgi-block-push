79d642bb4fb63aed597575e2e626e613b0d045ed
Brendan Bobryk <babobryk@gmail.com>
2026-02-07 10:16:54 -0800

    updated instruction prompts

--- corgi-block-push/src/components/Game.jsx
+++ corgi-block-push/src/components/Game.jsx
@@ -215,7 +215,7 @@ const Game = () => {
                 transition: "transform 0.3s ease, opacity 0.3s ease"
               }}
             >
-              {hasTreat ? "Get to the goal 🚩" : "Collect the treat 🦴"}
+              {hasTreat ? "Get to the goal! 🚩" : "Collect the treat! 🦴"}
             </div>
           </div>
 
