e9514b76ba9b378da1531ccb7d7d95fe73314fb4
Brendan Bobryk <babobryk@gmail.com>
2026-02-07 10:18:23 -0800

    updated level reset button

--- corgi-block-push/src/components/Game.jsx
+++ corgi-block-push/src/components/Game.jsx
@@ -180,7 +180,7 @@ const Game = () => {
           </div>
         ))}
 
-        <button onClick={() => resetGame()}>🔄 Reset Level</button>
+        <button onClick={() => resetGame()}>🔄 Reset Level 🔄</button>
 
         <button onClick={() => { localStorage.removeItem("bestMoves"); setBestMoves({}); }}>
           🗑️ Reset All Progress
